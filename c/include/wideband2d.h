dad9ccb98407143c9741f5ca4c8f56cf7bc61723
Mike O'Neil <oneil@cims.nyu.edu>
2021-10-08 13:35:02 -0400

    adding some c headers

--- c/include/wideband2d.h
+++ c/include/wideband2d.h
@@ -5,3 +5,13 @@ void h2dmpmphf_vec_(int *nd, double _Complex *zk, double *rscale0, double *cente
                   double *rscale1, double *center1,
                   double _Complex *mpole1, int *nterms1);
 
+void h2dmplochf_vec_(int *nd, double _Complex *zk, double *rscale0, double *center0,
+                  double _Complex *mpole0, int *nterms0,
+                  double *rscale1, double *center1,
+                  double _Complex *mpole1, int *nterms1);
+
+void h2dloclochf_vec_(int *nd, double _Complex *zk, double *rscale0, double *center0,
+                  double _Complex *mpole0, int *nterms0,
+                  double *rscale1, double *center1,
+                  double _Complex *mpole1, int *nterms1);
+
